bb5cb88982cfadb6ec2080dd862274d24678af12
Rajasree2004 <rajasree.selva@gmail.com>
2023-05-10 23:57:42 +0530

    class created and inti function defines

--- linear-reg-scratch.py
+++ linear-reg-scratch.py
@@ -1,4 +1,23 @@
 import numpy as np
 
-class Linear_Regression:
-    pass
+class Linear_Regression():
+    
+    #initiating parameters
+    def __init__(self, learning_rate, no_of_iterations):
+
+        self.learning_rate = learning_rate
+        self.no_of_iterations = no_of_iterations
+
+    
+    def fit(self, x, y):
+        
+        #no of trainig examples(m) and no of features(n)
+        self.m, self.n = x.shape
+
+    
+    def update_weight(self):
+        pass
+    
+    def predict(self):
+        pass
+    
