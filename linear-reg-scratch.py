25fa18560ce62aa37b80e6774dbfdbc729556ad5
Rajasree2004 <rajasree.selva@gmail.com>
2023-05-11 00:29:50 +0530

    done coding of linear regression

--- linear-reg-scratch.py
+++ linear-reg-scratch.py
@@ -13,11 +13,33 @@ class Linear_Regression():
         
         #no of trainig examples(m) and no of features(n)
         self.m, self.n = x.shape
+        #initiate weight and bias
+        self.w = np.zeros(self.n)
+        self.b = 0
+        self.x =x
+        self.y =y
 
-    
+
+        #implement gradient descent
+
+        for i in range(self.no_of_iterations):
+            self.update_weight()
+
+
+
+
     def update_weight(self):
-        pass
+        Y_prediction = self.predict(self.x)
+
+        #calculate gradient
+
+        dw = (-2*(self.x.T).dot(self.y - Y_prediction)) / self.m 
+        db = (-2*np.sum(self.y - Y_prediction)) / self.m
+
+        self.w = self.w - self.learning_rate*dw
+        self.b = self.b - self.learning_rate*db
     
-    def predict(self):
-        pass
-    
+    def predict(self, x):
+        
+        return x.dot(self.w) + self.b
+
